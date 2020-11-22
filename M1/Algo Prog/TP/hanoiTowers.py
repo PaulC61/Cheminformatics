2badf89b2f1be3118e532f76f14bf936a3d3d0b6
PaulC61 <paulclark61@gmail.com>
2020-11-22 19:53:52 +0100

    Added multipleDisk function to Towers of Hanoi, not recursive though. Had to modify the motion function for it to work and not break previously made functions

--- M1/Algo Prog/TP/hanoiTowers.py
+++ M1/Algo Prog/TP/hanoiTowers.py
@@ -1,6 +1,7 @@
 # departure tower
 # destination tower
 # third tower that is neither
+#%%
 towerlst = [[2],[3],[1]]
 
 def thirdTower(dep, dest):
@@ -14,10 +15,10 @@ def thirdTower(dep, dest):
 def displaysMovement(dep, dest):
     return print("T" + str(dep) + " --> T" + str(dest))
 
-def motion(towerlst, dep, dest):
+def motion(towerlst, dep, dest, nb_disques = 0):
     print(towerlst)
-    towerlst[dest] = [towerlst[dep][0]] + towerlst[dest]
-    towerlst[dep] = towerlst[dep][1:]
+    towerlst[dest] = towerlst[dep][0:nb_disques] + towerlst[dest]
+    towerlst[dep] = towerlst[dep][nb_disques:]
     displaysMovement(dep, dest)
     print(towerlst)
     return towerlst
@@ -25,7 +26,7 @@ def motion(towerlst, dep, dest):
 def movementIsValid(towerlst, dep, dest):
     # returns True if and only if deb and dest belong to {0,1,2}
     # all one statement?
-    return ((dep in [0, 1, 2] and dest in [0, 1, 2]) and (dep != dest) and (towerlst[dep] != []) and (towerlst[dep][0] < towerlst[dest][0] or towerlst[dest] == []))
+    return ((dep in [0, 1, 2] and dest in [0, 1, 2]) and (dep != dest) and (towerlst[dep] != []) and (towerlst[dest] == [] or (towerlst[dep][0] < towerlst[dest][0])))
 
 
     # dep and dest are different
@@ -33,8 +34,26 @@ def movementIsValid(towerlst, dep, dest):
     # the starting tower is not empty
     # the first disc of the starting tower is smaller 
     # than the first disc of the destination tower
+towerLst = [[1,2,3],[],[]]
 
+def movementMultiDisk(towerlst, dep, dest, nb_disques):
+    # checks movement from dep to dest is indeed valid: call isValid
+    # if nb_disques == 0; return towerlst
+    # if yes: move the upper disks of dep to dest: call move
+
+
+    if nb_disques == 0:
+        return towerlst
+    elif movementIsValid(towerlst, dep, dest) == True:
+
+        return motion(towerlst, dep, dest, nb_disques) #movementMultiDisk(motion(towerlst, dep, dest, nb_disques), dep, dest, nb_disques-1)
+    else:
+        displaysMovement(dep, dest)
+        print(towerlst)
+        return towerlst
 
 print(movementIsValid(towerlst, 2, 0))
 
-motion(towerlst,0,1)
+movementMultiDisk(towerLst, 0, 2, 1)
+
+# %%
