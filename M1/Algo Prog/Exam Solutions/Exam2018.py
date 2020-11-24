8e05a58164da120de87cbae9d9f76ce3983965a5
PaulC61 <paulclark61@gmail.com>
2020-11-24 11:42:34 +0100

    Question 1.2 complete: tested, last commit should have been Q1.1

--- M1/Algo Prog/Exam Solutions/Exam2018.py
+++ M1/Algo Prog/Exam Solutions/Exam2018.py
@@ -3,7 +3,7 @@
 # i) duplicates the inputs to accomplish the task
 # ii) uses a loop to add the characters sequentially
 
-# write a function "stars" that when inputting an int "lngth" produces a chaine of length lngth of "*"
+# 1.1 write a function "stars" that when inputting an int "lngth" produces a chaine of length lngth of "*"
 # i)
 def starsDup(lngth):
     return "*"*lngth
@@ -15,5 +15,25 @@ def starsLoop(lngth):
         starStrng += "*"
     return starStrng
 
-print(starsDup(5))
-print(starsLoop(5))
+# 1.2 write a function "starsBandWhite" 
+# inputs "lngth", "pos", "bandWdth"
+# return
+#   chain of characters of length "lngth"
+#   where characters are "*" except for a
+#   bandwidth "bandWdth" at starting positon "pos"
+# i)
+def starsBandWhiteDup(lngth, pos, bandWdth):
+    return "*"*pos + " "*bandWdth + "*"*(lngth-pos-bandWdth)
+
+#ii)
+def starsBandWhiteLoop(lngth, pos, bandWdth):
+    starStrng = ""
+    for i in range (lngth):
+        if i >= pos and i < pos+bandWdth:
+            starStrng += " " 
+        else:
+            starStrng += "*"
+    return starStrng
+
+print(starsBandWhiteDup(10, 3, 2))
+print(starsBandWhiteLoop(10, 3, 2))
