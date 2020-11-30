0c3d15bb36d0ad4007f867cc709ccf7320eea6dd
PaulC61 <paulclark61@gmail.com>
2020-11-30 12:01:36 +0100

    TP_Note, Question 1.4 nNextToQueenFini

--- M1/Algo Prog/TP_Note/paul.py
+++ M1/Algo Prog/TP_Note/paul.py
@@ -3,12 +3,18 @@ import graph
 def nextToQueen(row, column):
     return [(row-1,column), (row-1,column+1), (row,column+1), (row+1,column+1), (row+1,column), (row+1,column-1), (row,column-1), (row-1,column-1)]
 
-def nNextToQueen(row, column, n):
-    return [(row-n,column), (row-n,column+n), (row,column+n), (row+n,column+n), (row+n,column), (row+n,column-n), (row,column-n), (row-n,column-n)]
-
 def isOnChessboard(row,column):
     return (row >= 0 and row < 8) and (column >= 0 and column < 8)
 
-print(nNextToQueen(3,7,3))
-print(isOnChessboard(7,0))
+def nNextToQueenFini(row, column, n):
+    infinBoardLst = [(row-n,column), (row-n,column+n), (row,column+n), (row+n,column+n), (row+n,column), (row+n,column-n), (row,column-n), (row-n,column-n)]
+    restrBoardLst = []
+    for i in range(len(infinBoardLst)):
+        if isOnChessboard(infinBoardLst[i][0], infinBoardLst[i][1]) == True:
+            restrBoardLst += [infinBoardLst[i]]
+    return restrBoardLst
+
+
+
+print(nNextToQueenFini(3,7,3))
 
