dd5e54b81816f0853b752c78c28472348f01283e
PaulC61 <paulclark61@gmail.com>
2020-11-30 12:28:12 +0100

    Q1 of TP_Note Complete

--- M1/Algo Prog/TP_Note/paul.py
+++ M1/Algo Prog/TP_Note/paul.py
@@ -1,13 +1,11 @@
 import graph
 
-def nextToQueen(row, column):
-    return [(row-1,column), (row-1,column+1), (row,column+1), (row+1,column+1), (row+1,column), (row+1,column-1), (row,column-1), (row-1,column-1)]
 
 def isOnChessboard(row,column):
     return (row >= 0 and row < 8) and (column >= 0 and column < 8)
 
-def nNextToQueenFini(row, column, n):
-    infinBoardLst = [(row-n,column), (row-n,column+n), (row,column+n), (row+n,column+n), (row+n,column), (row+n,column-n), (row,column-n), (row-n,column-n)]
+def nextToQueenFini(row, column):
+    infinBoardLst=[(row-1,column), (row-1,column+1), (row,column+1), (row+1,column+1), (row+1,column), (row+1,column-1), (row,column-1), (row-1,column-1)]
     restrBoardLst = []
     for i in range(len(infinBoardLst)):
         if isOnChessboard(infinBoardLst[i][0], infinBoardLst[i][1]) == True:
@@ -15,6 +13,23 @@ def nNextToQueenFini(row, column, n):
     return restrBoardLst
 
 
+def N_ToQueenFini(row, column, n):
+    infinBoardLst = [(row-n,column), (row-n,column+n), (row,column+n), (row+n,column+n), (row+n,column), (row+n,column-n), (row,column-n), (row-n,column-n)]
+    restrBoardLst = []
+    for i in range(len(infinBoardLst)):
+        if isOnChessboard(infinBoardLst[i][0], infinBoardLst[i][1]) == True:
+            restrBoardLst += [infinBoardLst[i]]
+    return restrBoardLst
 
-print(nNextToQueenFini(3,7,3))
+def availableMoves(row, column):
+    lstAvailSpaces = []
+    for i in range(1, 8):
+        if N_ToQueenFini(row, column, i) != []:
+            lstAvailSpaces += N_ToQueenFini(row, column, i)
+        else:
+            return lstAvailSpaces
+
+
+
+print(availableMoves(3,2))
 
