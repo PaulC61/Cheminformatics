b0c51316f1d4ad01b501889e46e0fa7736172adf
PaulC61 <paulclark61@gmail.com>
2021-01-20 15:21:23 +0100

    Histogram set for rsmd, first draft

--- M1/Supramolecular_Modelling/matplotlib_basics.py
+++ M1/Supramolecular_Modelling/matplotlib_basics.py
@@ -3,14 +3,25 @@
 import numpy as np
 
 from matplotlib import pyplot as plt
+from matplotlib import mlab as mlab
 
 bnddata = np.genfromtxt('rmsd-bnd.txt', delimiter=' ')
 
-rmsd = bnddata[1:50,1]
+rmsd50 = bnddata[1:50,1]
+rmsd100 = bnddata[1:100,1]
+rmsd200 = bnddata[1:200,1]
+rmsd500 = bnddata[1:500,1]
+rmsd1000 = bnddata[1:1000,1]
+rmsd2000 = bnddata[1:2000,1]
 
-plt.hist(rmsd, bins=100, edgecolor='black')
+plt.hist(rmsd50, bins=100, edgecolor='black', histtype=u'step')
+plt.hist(rmsd100, bins=100, edgecolor='blue', histtype=u'step')
+plt.hist(rmsd200, bins=100, edgecolor='orange', histtype=u'step')
+plt.hist(rmsd500, bins=100, edgecolor='green', histtype=u'step')
+plt.hist(rmsd1000, bins=100, edgecolor='purple', histtype=u'step')
+plt.hist(rmsd2000, bins=100, edgecolor='red', histtype=u'step')
+
 
 plt.xlabel('rmsd')
-
 
 # %%
