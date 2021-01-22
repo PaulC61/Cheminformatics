6cde2d380e6acffbe703cd5bdff08747dacf5173
PolinaOleneva <oleneva.polina@list.ru>
2021-01-22 12:56:09 +0100

    Isolated probability distributions

--- M1/Supramolecular_Modelling/matplotlib_basics.py
+++ M1/Supramolecular_Modelling/matplotlib_basics.py
@@ -18,6 +18,7 @@ rmsd2000 = bnddata[1:2000,1]
 
 
 #to produce density distributions you need to add 'density=True' in each histogram
+
 data1,bins1,_=plt.hist(rmsd50, bins=100, edgecolor='black', histtype=u'step')
 data2,bins2,_=plt.hist(rmsd100, bins=100, edgecolor='blue', histtype=u'step')
 data3,bins3,_=plt.hist(rmsd200, bins=100, edgecolor='orange', histtype=u'step')
@@ -25,6 +26,11 @@ data4,bins4,_=plt.hist(rmsd500, bins=100, edgecolor='green', histtype=u'step')
 data5,bins5,_=plt.hist(rmsd1000, bins=100, edgecolor='purple', histtype=u'step')
 data6,bins6,_=plt.hist(rmsd2000, bins=100, edgecolor='red', histtype=u'step')
 
+
+
+# %%
+
+#%%
 density1=stats.gaussian_kde(rmsd50)
 density2=stats.gaussian_kde(rmsd100)
 density3=stats.gaussian_kde(rmsd200)
@@ -34,12 +40,12 @@ density6=stats.gaussian_kde(rmsd2000)
 
 
 plt.xlabel('rmsd')
-plt.plot(bins1, density1)
-plt.plot(bins2, density2)
-plt.plot(bins3, density3)
-plt.plot(bins4, density4)
-plt.plot(bins5, density5)
-plt.plot(bins6, density6)
+plt.plot(bins1, density1(bins1))
+plt.plot(bins2, density2(bins2))
+plt.plot(bins3, density3(bins3))
+plt.plot(bins4, density4(bins4))
+plt.plot(bins5, density5(bins5))
+plt.plot(bins6, density6(bins6))
 plt.show()
-
 # %%
+
