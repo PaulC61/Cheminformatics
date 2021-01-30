1d5ab3781e282e6b7713643fdb4ec35ed722cc1c
PaulC61 <paulclarke61@gmail.com>
2021-01-30 16:52:21 +0100

    Added frequency and density plots to G20 for original value of Kf in ll series

---
+++ M1/OO_Prog/TDs/shapes/Cone.java
@@ -0,0 +1,8 @@
+package M1.OO_Prog.TDs.shapes;
+
+public class Cone {
+    int h;
+    int r;
+    
+    public int 
+}
---
+++ M1/OO_Prog/TDs/shapes/Shape.java
@@ -0,0 +1,16 @@
+package M1.OO_Prog.TDs.shapes;
+
+public class Shape {
+    private int radius;
+    private int faceNumber;
+    private List<Integer> Lsides;
+    
+
+    public Shape (boolean hasRadiusIn, int faceNumberIn, List LsideIn){
+        hasRadius = hasRadiusIn;
+        faceNumber = faceNumberIn;
+        Lsides = LsideIn;
+    }
+
+
+}
