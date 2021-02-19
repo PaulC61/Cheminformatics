e69c18515b6f0ba81f7093d1046974189302cb59
PaulC61 <paulclarke61@gmail.com>
2021-02-19 13:26:59 +0100

    I dunno#

--- M1/OO_Prog/TDs/shapes/Cone.java
+++
@@ -1,8 +0,0 @@
-package M1.OO_Prog.TDs.shapes;
-
-public class Cone {
-    int h;
-    int r;
-    
-    public int 
-}
--- M1/OO_Prog/TDs/shapes/Pave.java
+++
@@ -1,5 +0,0 @@
-package M1.OO_Prog.TDs.shapes;
-
-public class Pave {
-    
-}
