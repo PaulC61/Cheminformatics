78c36cfaa6395ccfda559b27ab71752a7c3a7e6e
PaulC61 <paulclarke61@gmail.com>
2021-03-03 20:55:08 +0100

    this

--- M1/Supramolecular_Modelling/G20/Week5/w_R_vs_kappa.py
+++ M1/Supramolecular_Modelling/G20/Week5/w_R_vs_kappa.py
@@ -1,4 +1,4 @@
-#%%
+
 import matplotlib.pyplot as plt
 
 kappa = [10, 100, 1000, 10000, 1000000]
@@ -6,10 +6,9 @@ w_R = [3.63266, 7.72334, 11.81402, 15.90470, 19.99538]
 w_tot = [0.59831, 4.68899, 8.77967, 12.87035, 16.96103]
 
 plt.figure()
-plt.plot(kappa, w_R, label= r'$W_{rest}$')
-plt.plot(kappa, w_tot, label=r'$W_{tot}$')
-plt.xlabel('\u03BA')
+plt.plot(kappa, w_R, label= r'$W_{rest}$', marker='o')
+# plt.plot(kappa, w_tot, label=r'$W_{tot}$', marker='o')
+plt.xlabel(r'$k_{f}$')
 plt.ylabel('Energy (kcal/mol)')
 plt.legend(loc='lower right')
 plt.show()
-# %%
