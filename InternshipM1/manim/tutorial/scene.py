66fcc4b69308d0cb6efa00ff4e40caf920c11dd3
PaulC61 <paulclarke61@gmail.com>
2021-05-03 20:48:57 +0200

    First tutorial set finished

--- InternshipM1/manim/tutorial/scene.py
+++ InternshipM1/manim/tutorial/scene.py
@@ -2,13 +2,24 @@ from manim import *
 
 class SquareToCircle(Scene):
     def construct(self):
-        circle = Circle()
-        circle.set_fill(PINK, opacity=0.5)
+        circle = Circle().shift(LEFT)
+        square = Square().shift(UP)
+        triangle = Triangle().shift(RIGHT)
 
-        square = Square()
-        square.flip(RIGHT)
-        square.rotate(-3 * TAU / 8)
+        circle.set_stroke(color=GREEN, width=20)
+        square.set_fill(YELLOW, opacity=1.0)
+        triangle.set_fill(PINK, opacity=0.5)
 
-        self.play(Create(square))
-        self.play(Transform(square, circle))
-        self.play(FadeOut(circle))
+        self.add(circle, square, triangle)
+        self.wait(1)
+
+class SomeAnimations(Scene):
+    def construct(self):
+        square = Square().set_fill(RED, opacity=1.0)
+        self.add(square)
+
+        self.play(ApplyMethod(square.set_fill, WHITE))
+        self.wait(1)
+
+        self.play(ApplyMethod(square.shift, UP), run_time = 4)
+        self.wait(1)
